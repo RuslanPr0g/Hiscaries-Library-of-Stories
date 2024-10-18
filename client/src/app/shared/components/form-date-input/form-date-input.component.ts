f953e64b3d13ad953e46b0a63edfc3ff7f394bc0
Ruslan Rudenko <theruslan.prog@gmail.com>
2024-10-18 15:28:33 +0200

    style fixes

--- client/src/app/shared/components/form-date-input/form-date-input.component.ts
+++ client/src/app/shared/components/form-date-input/form-date-input.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CalendarModule } from 'primeng/calendar';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'app-form-date-input',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, CalendarModule],
+  imports: [CommonModule, ReactiveFormsModule, CalendarModule, MessageModule],
   templateUrl: './form-date-input.component.html',
   styleUrls: ['./form-date-input.component.scss']
 })
