8451d03b798dca86b375cfaa418a56be100d8b78
Ruslan Rudenko <theruslan.prog@gmail.com>
2024-10-21 15:53:07 +0200

    add carousel

--- client/src/app/stories/search-story-results/search-story-results.component.ts
+++ client/src/app/stories/search-story-results/search-story-results.component.ts
@@ -5,11 +5,12 @@ import { take } from 'rxjs';
 import { StoryModel } from '../models/domain/story-model';
 import { SearchStoryItemComponent } from '../story-search-item/story-search-item.component';
 import { SkeletonModule } from 'primeng/skeleton';
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'app-search-story-results',
   standalone: true,
-  imports: [CommonModule, SearchStoryItemComponent, SkeletonModule],
+  imports: [CommonModule, SearchStoryItemComponent, SkeletonModule, CarouselModule],
   templateUrl: './search-story-results.component.html',
   styleUrl: './search-story-results.component.scss'
 })
@@ -17,23 +18,44 @@ export class SearchStoryResultsComponent {
   stories: StoryModel[] = [];
 
   @Input() initialStories: StoryModel[];
-  
+  @Input() isCarousel: boolean = false;
+
+  responsiveOptions: any[] | undefined;
+
   constructor(
     private storyService: StoryService
   ) {
     if (!this.initialStories || this.initialStories.length === 0) {
       this.storyService.recommendations()
-      .pipe(take(1))
-      .subscribe(stories => {
-        this.stories = stories.map(s => ({
-          ...s,
-          ImagePreview: this.convertToBase64(s.ImagePreview)
-        }));
-      });
+        .pipe(take(1))
+        .subscribe(stories => {
+          this.stories = stories.map(s => ({
+            ...s,
+            ImagePreview: this.convertToBase64(s.ImagePreview)
+          }));
+        });
     }
+
+    this.responsiveOptions = [
+      {
+        breakpoint: '1950px',
+        numVisible: 2,
+        numScroll: 1
+      },
+      {
+        breakpoint: '1150px',
+        numVisible: 1,
+        numScroll: 1
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 1,
+        numScroll: 1
+      }
+    ];
   }
 
   private convertToBase64(byteArray: any): string {
-      return 'data:image/jpeg;base64,' + byteArray;
+    return 'data:image/jpeg;base64,' + byteArray;
   }
 }
