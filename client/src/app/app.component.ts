9827b1715e84fbba67ff1014a95a199fcb3efe2e
Ruslan Rudenko <theruslan.prog@gmail.com>
2024-10-24 18:36:21 +0200

    fixes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { LoadingOverlayComponent } from './shared/components/loading-overlay/loading-overlay.component';
 import { CommonModule } from '@angular/common';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { NavigationConst } from './shared/constants/navigation.const';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, LoadingOverlayComponent],
+  imports: [CommonModule, RouterOutlet, ButtonModule, SidebarModule, HeaderComponent, LoadingOverlayComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'hiscaries';
   loading: boolean = true;
+  sidebarVisible: boolean = false;
+
+  constructor(
+    private router: Router
+  ) {
+  }
 
   ngOnInit() {
     setTimeout(() => {
@@ -23,5 +32,9 @@ export class AppComponent {
 
   fadeOutLoading() {
     this.loading = false;
+  }
+
+  home(): void {
+    this.router.navigate([NavigationConst.Home]);
   }
 }
