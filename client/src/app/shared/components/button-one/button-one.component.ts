036110a17e2c1d98614557f73da6c6c01aa85d5a
Ruslan Rudenko <theruslan.prog@gmail.com>
2024-11-01 11:28:35 +0100

    fix recommendation section

---
+++ client/src/app/shared/components/button-one/button-one.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+    selector: 'app-button-one',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './button-one.component.html',
+    styleUrls: ['./button-one.component.scss'],
+})
+export class ButtonOneComponent {
+    @Input() label?: string;
+    @Output() clickAction = new EventEmitter<void>();
+
+    click(): void {
+        this.clickAction?.emit();
+    }
+}
--- client/src/app/stories/search-story-results/search-story-results.component.ts
+++ client/src/app/stories/search-story-results/search-story-results.component.ts
@@ -1,37 +1,50 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { StoryService } from '../services/story.service';
 import { take } from 'rxjs';
 import { StoryModel } from '../models/domain/story-model';
 import { SearchStoryItemComponent } from '../story-search-item/story-search-item.component';
 import { SkeletonModule } from 'primeng/skeleton';
 import { CarouselModule, CarouselResponsiveOptions } from 'primeng/carousel';
+import { ButtonOneComponent } from '../../shared/components/button-one/button-one.component';
 
 @Component({
     selector: 'app-search-story-results',
     standalone: true,
-    imports: [CommonModule, SearchStoryItemComponent, SkeletonModule, CarouselModule],
+    imports: [CommonModule, SearchStoryItemComponent, SkeletonModule, CarouselModule, ButtonOneComponent],
     templateUrl: './search-story-results.component.html',
-    styleUrl: './search-story-results.component.scss',
+    styleUrls: ['./search-story-results.component.scss'],
 })
-export class SearchStoryResultsComponent {
-    stories: StoryModel[] = [];
+export class SearchStoryResultsComponent implements OnInit {
+    private _stories: StoryModel[] = [];
+    private readonly _chunkSize: number = 6;
 
-    @Input() initialStories: StoryModel[];
+    @Input() initialStories: StoryModel[] = [];
     @Input() isCarousel: boolean = false;
 
     responsiveOptions: CarouselResponsiveOptions[] | undefined;
 
-    constructor(private storyService: StoryService) {
-        if (!this.initialStories || this.initialStories.length === 0) {
-            this.storyService
-                .recommendations()
-                .pipe(take(1))
-                .subscribe((stories) => {
-                    this.stories = stories;
-                });
+    constructor(private storyService: StoryService) {}
+
+    ngOnInit(): void {
+        this._stories = this.initialStories.length > 0 ? this.initialStories : [];
+        if (this._stories.length === 0) {
+            this.fetchRecommendations();
         }
 
+        this.initializeResponsiveOptions();
+    }
+
+    private fetchRecommendations(): void {
+        this.storyService
+            .recommendations()
+            .pipe(take(1))
+            .subscribe((stories) => {
+                this._stories = [...this._stories, ...stories];
+            });
+    }
+
+    private initializeResponsiveOptions(): void {
         this.responsiveOptions = [
             {
                 breakpoint: '1950px',
@@ -49,5 +62,17 @@ export class SearchStoryResultsComponent {
                 numScroll: 1,
             },
         ];
+    }
+
+    get displayedStories(): StoryModel[] {
+        return this._stories.slice(0, this._chunkSize);
+    }
+
+    showMore(): void {
+        this._stories.splice(0, this._chunkSize);
+
+        if (this._stories.length < 12) {
+            this.fetchRecommendations();
+        }
     }
 }
