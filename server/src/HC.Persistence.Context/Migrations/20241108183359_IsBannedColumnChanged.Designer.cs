0bc33452379dd42c483a0ea2e961e40728159dc1
Ruslan Rudenko <theruslan.prog@gmail.com>
2024-11-08 19:34:18 +0100

    fixes

--- server/src/HC.Persistence.Context/Migrations/20241108183359_IsBannedColumnChanged.Designer.cs
+++ server/src/HC.Persistence.Context/Migrations/20241108183359_IsBannedColumnChanged.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HC.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace HC.Persistence.Context.Migrations
 {
     [DbContext(typeof(HiscaryContext))]
-    partial class HiscaryContextModelSnapshot : ModelSnapshot
+    [Migration("20241108183359_IsBannedColumnChanged")]
+    partial class IsBannedColumnChanged
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
