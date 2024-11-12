73ac32b83581de36a67a2fd7c745e851290a53de
Ruslan Rudenko <theruslan.prog@gmail.com>
2024-11-12 13:34:11 +0100

    5 > Reading progress is added

--- server/src/HC.Domain/Users/UserReadHistory.cs
+++ server/src/HC.Domain/Users/UserReadHistory.cs
@@ -20,17 +20,8 @@ public sealed class UserReadHistory : Entity<UserReadHistoryId>
 
     public UserId UserId { get; init; }
     public StoryId StoryId { get; init; }
-    public Story Story { get; }
     public int LastPageRead { get; private set; }
     public bool SoftDeleted { get; init; }
-
-    public decimal CalculatePercentageRead()
-    {
-        int totalStoryPage = Story.Contents.Count;
-        int currentLastPage = LastPageRead;
-
-        return (currentLastPage / totalStoryPage) * 100;
-    }
 
     internal void ReadPage(int page)
     {
