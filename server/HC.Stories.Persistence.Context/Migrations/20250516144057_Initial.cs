404818f9aa40f94bef11d6da3467c28a5e78fa24
Ruslan Rudenko <theruslan.prog@gmail.com>
2025-05-16 16:42:51 +0200

    Add migrations for every single context

---
+++ server/HC.Stories.Persistence.Context/Migrations/20250516144057_Initial.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HC.Stories.Persistence.Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "stories");
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                schema: "stories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    ImagePreview = table.Column<byte[]>(type: "bytea", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EditedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Version = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OutboxMessages",
+                schema: "stories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    OccuredOnUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ProcessedOnUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Error = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutboxMessages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stories",
+                schema: "stories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    LibraryId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    AuthorName = table.Column<string>(type: "text", nullable: false),
+                    AgeLimit = table.Column<int>(type: "integer", nullable: false),
+                    DateWritten = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ImagePreviewUrl = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EditedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Version = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                schema: "stories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    StoryId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PlatformUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    Score = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EditedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Version = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_Stories_StoryId",
+                        column: x => x.StoryId,
+                        principalSchema: "stories",
+                        principalTable: "Stories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StoryAudios",
+                schema: "stories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FileId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    StoryId = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EditedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Version = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoryAudios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StoryAudios_Stories_StoryId",
+                        column: x => x.StoryId,
+                        principalSchema: "stories",
+                        principalTable: "Stories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StoryGenres",
+                schema: "stories",
+                columns: table => new
+                {
+                    GenresId = table.Column<Guid>(type: "uuid", nullable: false),
+                    StoryId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoryGenres", x => new { x.GenresId, x.StoryId });
+                    table.ForeignKey(
+                        name: "FK_StoryGenres_Genres_GenresId",
+                        column: x => x.GenresId,
+                        principalSchema: "stories",
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StoryGenres_Stories_StoryId",
+                        column: x => x.StoryId,
+                        principalSchema: "stories",
+                        principalTable: "Stories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StoryPages",
+                schema: "stories",
+                columns: table => new
+                {
+                    StoryId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Page = table.Column<int>(type: "integer", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EditedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Version = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoryPages", x => new { x.StoryId, x.Page });
+                    table.ForeignKey(
+                        name: "FK_StoryPages_Stories_StoryId",
+                        column: x => x.StoryId,
+                        principalSchema: "stories",
+                        principalTable: "Stories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StoryRatings",
+                schema: "stories",
+                columns: table => new
+                {
+                    StoryId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PlatformUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Score = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EditedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Version = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoryRatings", x => new { x.StoryId, x.PlatformUserId });
+                    table.ForeignKey(
+                        name: "FK_StoryRatings_Stories_StoryId",
+                        column: x => x.StoryId,
+                        principalSchema: "stories",
+                        principalTable: "Stories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_StoryId",
+                schema: "stories",
+                table: "Comments",
+                column: "StoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoryAudios_StoryId",
+                schema: "stories",
+                table: "StoryAudios",
+                column: "StoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoryGenres_StoryId",
+                schema: "stories",
+                table: "StoryGenres",
+                column: "StoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments",
+                schema: "stories");
+
+            migrationBuilder.DropTable(
+                name: "OutboxMessages",
+                schema: "stories");
+
+            migrationBuilder.DropTable(
+                name: "StoryAudios",
+                schema: "stories");
+
+            migrationBuilder.DropTable(
+                name: "StoryGenres",
+                schema: "stories");
+
+            migrationBuilder.DropTable(
+                name: "StoryPages",
+                schema: "stories");
+
+            migrationBuilder.DropTable(
+                name: "StoryRatings",
+                schema: "stories");
+
+            migrationBuilder.DropTable(
+                name: "Genres",
+                schema: "stories");
+
+            migrationBuilder.DropTable(
+                name: "Stories",
+                schema: "stories");
+        }
+    }
+}
