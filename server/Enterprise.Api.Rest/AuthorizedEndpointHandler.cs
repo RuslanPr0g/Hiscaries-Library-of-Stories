ba99a02c0771cd852e5461b1e5c8ec8f8043f005
Ruslan Rudenko <theruslan.prog@gmail.com>
2025-05-23 16:28:39 +0200

    Handle user context fixes

--- server/Enterprise.Api.Rest/AuthorizedEndpointHandler.cs
+++ server/Enterprise.Api.Rest/AuthorizedEndpointHandler.cs
@@ -9,84 +9,77 @@ public class AuthorizedEndpointHandler(IHttpContextAccessor contextAccessor) : I
     public async Task<IResult> WithUser<TActionResult>(Func<CurrentUser, Task<TActionResult>> action)
         where TActionResult : class?
     {
-        var context = _contextAccessor.HttpContext;
-
-        if (context is null)
-        {
-            return Results.BadRequest("Something went wrong when processing the http request.");
-        }
-
-        var user = CurrentUser.FromClaims(context.User);
-
-        if (user is null)
-        {
-            return Results.BadRequest("Unable to interpret the provided token.");
-        }
-
-        var result = await action(user);
-        return result.ToHttpResult();
+        return await HandleUserContextAsync(
+            _contextAccessor.HttpContext,
+            action,
+            result => result.ToHttpResult());
     }
 
     public IResult WithUser<TActionResult>(Func<CurrentUser, TActionResult> action)
         where TActionResult : class?
     {
-        var context = _contextAccessor.HttpContext;
+        return HandleUserContext(
+            _contextAccessor.HttpContext,
+            action,
+            result => result.ToHttpResult());
+    }
 
+    public async Task<IResult> WithUserOperation<TActionResult>(Func<CurrentUser, Task<TActionResult>> action)
+        where TActionResult : OperationResult?
+    {
+        return await HandleUserContextAsync(
+            _contextAccessor.HttpContext,
+            action,
+            result => result.OperationToHttpResult());
+    }
+
+    public async Task<IResult> WithUserOperation<TActionResultValue>(
+        Func<CurrentUser, Task<OperationResult<TActionResultValue>>> action)
+        where TActionResultValue : class
+    {
+        return await HandleUserContextAsync(
+            _contextAccessor.HttpContext,
+            action,
+            result => result.OperationToHttpResult());
+    }
+
+    private static IResult HandleUserContext<T>(
+        HttpContext? context,
+        Func<CurrentUser, T> action,
+        Func<T, IResult> resultConverter)
+    {
         if (context is null)
         {
             return Results.BadRequest("Something went wrong when processing the http request.");
         }
 
         var user = CurrentUser.FromClaims(context.User);
-
         if (user is null)
         {
             return Results.BadRequest("Unable to interpret the provided token.");
         }
 
         var result = action(user);
-        return result.ToHttpResult();
+        return resultConverter(result);
     }
 
-    public async Task<IResult> WithUserOperation<TActionResult>(Func<CurrentUser, Task<TActionResult>> action)
-        where TActionResult : OperationResult?
+    private static async Task<IResult> HandleUserContextAsync<T>(
+        HttpContext? context,
+        Func<CurrentUser, Task<T>> action,
+        Func<T, IResult> resultConverter)
     {
-        var context = _contextAccessor.HttpContext;
-
         if (context is null)
         {
             return Results.BadRequest("Something went wrong when processing the http request.");
         }
 
         var user = CurrentUser.FromClaims(context.User);
-
         if (user is null)
         {
             return Results.BadRequest("Unable to interpret the provided token.");
         }
 
         var result = await action(user);
-        return result.OperationToHttpResult();
-    }
-
-    public IResult WithUserOperation<TActionResult>(Func<CurrentUser, TActionResult> action)
-        where TActionResult : OperationResult?
-    {
-        var context = _contextAccessor.HttpContext;
-
-        if (context is null)
-        {
-            return Results.BadRequest("Something went wrong when processing the http request.");
-        }
-
-        var user = CurrentUser.FromClaims(context.User);
-
-        if (user is null)
-        {
-            return Results.BadRequest("Unable to interpret the provided token.");
-        }
-
-        var result = action(user);
-        return result.OperationToHttpResult();
+        return resultConverter(result);
     }
 }
--- server/Enterprise.Api.Rest/IAuthorizedEndpointHandler.cs
+++ server/Enterprise.Api.Rest/IAuthorizedEndpointHandler.cs
@@ -4,15 +4,16 @@ namespace Enterprise.Api.Rest;
 
 public interface IAuthorizedEndpointHandler
 {
-    public Task<IResult> WithUser<TActionResult>(Func<CurrentUser, Task<TActionResult>> action)
+    Task<IResult> WithUser<TActionResult>(Func<CurrentUser, Task<TActionResult>> action)
         where TActionResult : class?;
 
-    public IResult WithUser<TActionResult>(Func<CurrentUser, TActionResult> action)
+    IResult WithUser<TActionResult>(Func<CurrentUser, TActionResult> action)
         where TActionResult : class?;
 
-    public Task<IResult> WithUserOperation<TActionResult>(Func<CurrentUser, Task<TActionResult>> action)
+    Task<IResult> WithUserOperation<TActionResult>(Func<CurrentUser, Task<TActionResult>> action)
         where TActionResult : OperationResult?;
 
-    public IResult WithUserOperation<TActionResult>(Func<CurrentUser, TActionResult> action)
-        where TActionResult : OperationResult?;
+    Task<IResult> WithUserOperation<TActionResultValue>(
+        Func<CurrentUser, Task<OperationResult<TActionResultValue>>> action)
+        where TActionResultValue : class;
 }
