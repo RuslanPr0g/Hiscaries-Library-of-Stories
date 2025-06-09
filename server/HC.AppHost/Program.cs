33fc1459c334fbf15d706e8c13bd44255ba036b3
Ruslan Rudenko <theruslan.prog@gmail.com>
2025-06-09 17:17:32 +0200

    f

--- server/HC.AppHost/Program.cs
+++ server/HC.AppHost/Program.cs
@@ -26,6 +26,7 @@ var azBlobs = builder.AddAzureStorage("azstorage")
                 .WithDataVolume()
     // .WithLifetime(ContainerLifetime.Persistent) // AVAILABLE SINCE .NET9 {SAD emoji}
     // .WaitFor // same fuck
+    // we need to move to .net9 but for that MassTransit has to be copied locally to work properly
     )
     .AddBlobs("azblobs");
 
