3afac1d6b790c2f8e201f1a31c808f3f88432e76
Ruslan Rudenko <theruslan.prog@gmail.com>
2025-06-26 11:09:06 +0200

    Fix apphost file to include the new project naming convention

--- server/Hiscary.AppHost/Program.cs
+++ server/Hiscary.AppHost/Program.cs
@@ -28,37 +28,37 @@ var azBlobs = builder.AddAzureStorage("azstorage")
     )
     .AddBlobs("azblobs");
 
-var useraccounts = builder.AddProject<Projects.HC_UserAccounts_Api_Rest>("hc-useraccounts-api-rest")
+var useraccounts = builder.AddProject<Projects.Hiscary_UserAccounts_Api_Rest>("hc-useraccounts-api-rest")
     .WithJwtAndSaltSettings(builder.Configuration)
     .WithHttpsEndpoint(name: "rest", port: 7010, targetPort: 7010, isProxied: false)
     .WithReference(postgres)
     .WithReference(rabbitmq)
     .WithReference(azBlobs);
-var notifications = builder.AddProject<Projects.HC_Notifications_Api_Rest>("hc-notifications-api-rest")
+var notifications = builder.AddProject<Projects.Hiscary_Notifications_Api_Rest>("hc-notifications-api-rest")
     .WithJwtAndSaltSettings(builder.Configuration)
     .WithHttpsEndpoint(name: "rest", port: 7011, targetPort: 7011, isProxied: false)
     .WithReference(postgres)
     .WithReference(rabbitmq)
     .WithReference(azBlobs);
-var platformusers = builder.AddProject<Projects.HC_PlatformUsers_Api_Rest>("hc-platformusers-api-rest")
+var platformusers = builder.AddProject<Projects.Hiscary_PlatformUsers_Api_Rest>("hc-platformusers-api-rest")
     .WithJwtAndSaltSettings(builder.Configuration)
     .WithHttpsEndpoint(name: "rest", port: 7012, targetPort: 7012, isProxied: false)
     .WithReference(postgres)
     .WithReference(rabbitmq)
     .WithReference(azBlobs);
-var stories = builder.AddProject<Projects.HC_Stories_Api_Rest>("hc-stories-api-rest")
+var stories = builder.AddProject<Projects.Hiscary_Stories_Api_Rest>("hc-stories-api-rest")
     .WithJwtAndSaltSettings(builder.Configuration)
     .WithHttpsEndpoint(name: "rest", port: 7013, targetPort: 7013, isProxied: false)
     .WithReference(postgres)
     .WithReference(rabbitmq)
     .WithReference(azBlobs);
-var media = builder.AddProject<Projects.HC_Media_Api_Rest>("hc-media-api-rest")
+var media = builder.AddProject<Projects.Hiscary_Media_Api_Rest>("hc-media-api-rest")
     .WithJwtAndSaltSettings(builder.Configuration)
     .WithHttpsEndpoint(name: "rest", port: 7014, targetPort: 7014, isProxied: false)
     .WithReference(rabbitmq)
     .WithReference(azBlobs);
 
-builder.AddProject<Projects.HC_LocalApiGateway>("hc-localapigateway")
+builder.AddProject<Projects.Hiscary_LocalApiGateway>("hc-localapigateway")
     .WithHttpsEndpoint(name: "apigateway", port: 5001, targetPort: 5001, isProxied: false)
     .WithReference(useraccounts)
     .WithReference(notifications)
